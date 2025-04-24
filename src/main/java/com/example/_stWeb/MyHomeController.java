2ab85b2a4939e61398f50cca15f7af0fd877813b
JavoT1 <javierignaciogalarcem@gmail.com>
2025-04-24 15:04:40 -0400

    Primer proyecto Spring

---
+++ src/main/java/com/example/_stWeb/MyHomeController.java
@@ -0,0 +1,23 @@
+package com.example._stWeb;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class MyHomeController {
+    @GetMapping("/")
+    public String index() {
+        return "Esta es Mi Primera Web";
+    }
+    @GetMapping("/api")
+    public String otro() {
+        return "Esta es Mi Segunda Web";
+    }
+    @GetMapping("/api/valor")
+    public String valor(@RequestParam int num1) {
+        return "El valor ingresado es " + num1;
+    }
+    
+}
